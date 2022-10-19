44517103a3b6c774f201aa869e9cd8ef536d06a4
Muhammad Usman <ch.usman564@gmail.com>
2022-10-19 14:22:49 +0500

    Built Header and Hero Section Skeleton

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,3 +4,10 @@ module.exports = {
   plugins: [],
 };
 
+
+// PRIMARY ORAGE #f79400
+// SECONDARY BLUE #316094
+// SECONDARY BLUE DARK #1e4571
+// TEXT_GRAY #766d66
+// TEXT_BLACK #111111
+// EASE cubic-bezier(.4,0,.2,1)
