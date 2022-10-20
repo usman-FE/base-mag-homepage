060664122927bc3cb6deabc5d69101416fc8fe7d
Muhammad Usman <ch.usman564@gmail.com>
2022-10-20 09:55:14 +0500

    Built Search Modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,3 +11,14 @@ module.exports = {
 // TEXT_GRAY #766d66
 // TEXT_BLACK #111111
 // EASE cubic-bezier(.4,0,.2,1)
+// DIRT font-['Russo_One',_sans-serif]
+// PRIMARY font-['Jost',_sans-serif]
+
+
+
+// // BODY OVERFLOW
+// bodyOverflow: function (modal) {
+//   document.querySelector('body').classList.remove('overflow-hidden', 'overflow-visible');
+//   document.querySelector('body').classList.add(modal ? 'overflow-hidden' : 'overflow-visible');
+// }
+// // END
