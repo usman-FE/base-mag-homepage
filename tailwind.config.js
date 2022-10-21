d88835ea0dc48dc049ea0d418d6a0e5bc01e8a78
Muhammad Usman <ch.usman564@gmail.com>
2022-10-21 12:53:18 +0500

    Built NEWS Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
 // SECONDARY BLUE #316094
 // SECONDARY BLUE DARK #1e4571
 // TEXT_GRAY #766d66
+// TEXT_GRAY 2 #454545
 // TEXT_BLACK #111111
 // EASE cubic-bezier(.4,0,.2,1)
 // SIDEBAR cubic-bezier(.86,.01,.77,.78)
